64254c90bd64b81e8b0b45c7f7b71c3ea1722731
Adel Urazov <adel.i.urazov@ya.ru>
2021-05-22 19:20:53 +0300

    Added LetStatement to cmakefile
    Signed-off-by: Adel Urazov <adel.i.urazov@ya.ru>

--- ast/statement/LetStatement.hpp
+++ ast/statement/LetStatement.hpp
@@ -6,5 +6,9 @@
 #include "../expression/Identifier.hpp"
 
 class LetStatement : public Statement {
+private:
   shared_ptr<Identifier> identifier;
+
+public:
+  LetStatement(const shared_ptr<Token> &token) : Statement(token) {}
 };
